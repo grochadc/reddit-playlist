4c09e286f8e9c68dc3f20b234d953453c5f9388e
Gonzalo Rocha <grochadc@gmail.com>
2018-11-06 11:30:35 -0600

    Style app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import YouTube from "react-youtube";
 import useVideoQueue from "./hooks";
+import "./App.css";
 
 function App() {
   const subreddit = window.location.pathname.substr(1);
@@ -12,6 +13,7 @@ function App() {
       videoId={currentVideo}
       onEnd={() => nextVideo()}
       opts={{ playerVars: { autoplay: 1 } }}
+      containerClassName="videoContainer"
     />
   );
 }
