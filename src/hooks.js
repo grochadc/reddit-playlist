3e1ccbc7ad9456b522e06708e2fed82ef62ce5c3
Gonzalo Rocha <grochadc@gmail.com>
2018-11-06 11:09:20 -0600

    Returns a local reddit file if no network

--- src/hooks.js
+++ src/hooks.js
@@ -1,25 +1,30 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import getYoutubeId from "./lib/getYoutubeId";
+const treemusic = require("./treemusic.json");
 
 export default function useVideoQueue(subreddit) {
   const [queue, setQueue] = useState([]);
   const [index, setIndex] = useState(0);
   const increment = () => setIndex(index + 1);
-  useEffect(async () => {
-    const { data } = await axios(`https://www.reddit.com/r/${subreddit}.json`);
-    const links = data.data.children
-      .filter(
-        ({ data }) =>
-          data.domain === "youtu.be" || data.domain === "youtube.com"
-      )
-      .map(
-        ({ data }) =>
-          data.domain === "youtu.be"
-            ? data.url.substr(data.url.lastIndexOf("/") + 1)
-            : getYoutubeId(decodeURIComponent(data.url))
-      );
-    setQueue(links);
+  useEffect(() => {
+    axios(`https://www.reddit.com/${subreddit}.json`)
+      .then(({ data }) => data)
+      .catch(() => treemusic)
+      .then(({ data }) => {
+        const links = data.children
+          .filter(
+            ({ data }) =>
+              data.domain === "youtu.be" || data.domain === "youtube.com"
+          )
+          .map(
+            ({ data }) =>
+              data.domain === "youtu.be"
+                ? data.url.substr(data.url.lastIndexOf("/") + 1)
+                : getYoutubeId(decodeURIComponent(data.url))
+          );
+        setQueue(links);
+      });
   }, []);
   return [queue[index], increment];
 }
